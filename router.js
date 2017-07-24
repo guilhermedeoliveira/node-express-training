3ade7714e985ea75e18ddf10a214bf6c833b6ed8
guilhermedeoliveira <contatoguilhermedeoliveira@gmail.com>
2017-07-24 18:17:42 -0300

    Wire up JwtStrategy to passport and add middleware to node route

--- router.js
+++ router.js
@@ -1,5 +1,13 @@
+const passport = require('passport');
+
 const auth = require('./controllers/auth');
+const passportService = require('./services/passport');
+
+const requireAuth = passport.authenticate('jwt', { session: false });
 
 module.exports = function(app) {
+  app.get('/', requireAuth, function(req, res) {
+    res.send({ auth: true, hi: 'There' });
+  });
   app.post('/signup', auth.signUp);
 }
